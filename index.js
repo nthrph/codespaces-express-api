3d9674e3d364d35c082b4c25f4f51483081ff2e2
nitharee <91041973+nthrph@users.noreply.github.com>
2024-10-17 08:47:12 +0000

    imprement backend for add /delete/update

--- index.js
+++ index.js
@@ -1,55 +1,9 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const app = express()
-// const port = 5000
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:false}));
-
-// app.use((req,res,next)=>{
-//   res.setHeader("Access-Control-Allow-Origin","*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept , Authorization",
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET , POST , PUT , DELETE"
-//   );
-//   next();
-// })
-
-// app.use(express.json());
-// const products=[
-//   {id:0,name:"Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-//   {id:1,name:"Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-//   {id:2,name:"Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-//   {id:3,name:"Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-//   {id:4,name:"Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-//   {id:5,name:"Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.get('/api/products',(req,res)=>{
-//   if(products.length>0)
-//     res.send(products);
-//   else
-//   res.status(400).send("No product founds");
-// });
-
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
 const express = require('express');
 const bodyParser = require('body-parser');
 var mysql = require('mysql');
-// const {application} = require('express');
-// const {v4 : uuidv4} = require('uuid');
+
 const app = express();
+const port = 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -57,11 +11,11 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept , Authorization",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET , POST , PUT , DELETE"
+    "GET, POST, PUT, DELETE",
   );
   next();
 });
@@ -71,37 +25,85 @@ var con = mysql.createConnection({
   host: "korawit.ddns.net",
   user: "webapp",
   password: "secret2024",
-  port: 3307,
-  database: "shop"
+  port: "3307",
+  database: "shop",
 });
+
 con.connect(function (err) {
   if (err) throw err;
 });
 
-app.get('/api/product', function (req, res) {
-  con.query("SELECT * FROM products", function (err, result, fields) {
-    if (err) throw res.status(400).send('Not found any product');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.get('/api/products', (req, res) => {
+  con.query("SELECT * FROM products_6430300498", function (err, result, fields) {
+    if (err) throw res.status(400).send("No products found");
     console.log(result);
     res.send(result);
   });
-});
+})
 
-app.get('/api/products/:id', function (req, res) {
+app.get('/api/products/:id', (req, res) => {
   const id = req.params.id;
-  con.query("SELECT * FROM products where id=" + id, function (err, result, fields) {
+  con.query(`SELECT * FROM products_6430300498 where id=${id}`, function (err, result, fields) {
     if (err) throw err;
-    let product = result;
-    if (product.length > 0) {
-      res.send(product);
-    } else {
-      res.status(400).send('Not found product for' + id)
+    if (result.length == 0)
+      res.status(400).send(`No products id: ${id} found`);
+    else {
+      console.log(result);
+      res.send(result);
     }
-    console.log(result);
   });
-});
+})
 
-const port = process.env.port || 3001;
+app.post('/api/addproduct' , (req,res)=>{
+  const name = req.body.name;
+  const price = req.body.price;
+  const img = req.body.img;
+  console.log( name,price,img);
+  con.query(`INSERT INTO products_6430300498 (name,price,img) VALUES ('${name}', '${price}' , '${img}')`, function (err, result, fields) {
+    if (err) throw err;
+    if (result.length == 0)
+      res.status(400).send(`No products id: ${id} found`);
+    else {
+      console.log(result);
+      res.send({products:result,status:"ok"});
+    }
+  });
+})
+
+app.delete('/api/delproduct/:id' , (req,res)=>{
+  const id =req.params.id;
+  con.query(`DELETE FROM * FROM products_6430300498 where id=${id}`, function (err, result, fields) {
+    if (err) throw err.status(400).send("Error, cannot delete product");
+    con.query("SELECT * FROM products_6430300498" , function (err,result,fields){
+      if(err) throw res.status(400).send("No products found");
+      console.log(result)
+      res.send({products:result,status:"ok"})
+    })
+  });
+})
+
+app.post('/api/updateproduct' , (req,res)=>{
+  const id =req.params.id;
+  const name = req.body.name;
+  const price = req.body.price;
+  const img = req.body.img;
+  console.log( name,price,img);
+  con.query(`UPDATE products_6430300498 SET name='${name}', price='${price}' , img='${img}' WHERE id = ${id})`, function (err, result, fields) {
+    if (err) throw err;
+    if (result.length == 0)
+      res.status(400).send(`No products id: ${id} found`);
+    else {
+      console.log(result);
+      res.send({products:result,status:"ok"});
+    }
+  });
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
